009d5fa63ab16b73219b4400e4aba9331272b944
Myabadhon <myabadhon@gmail.com>
2022-02-14 13:45:32 +0600

    test branch

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import sys
 import cherrypy
-
 from mako.template import Template
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
 
-class Todo(object):
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://127.0.0.1:8080/'))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+# app = QApplication(sys.argv)
+# QApplication.setApplicationName('TODO')
+# window = MainWindow()
+# app.exec_()
+class Todo(MainWindow):
     @cherrypy.expose
     def index(self):
 
@@ -13,7 +27,6 @@ class Todo(object):
     @cherrypy.expose
     def store(self, desc, date):
         return desc
-
 
 if __name__ == '__main__':
     cherrypy.quickstart(Todo(), '/')
